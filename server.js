36eccc0d55b3c25e9883eec6aa0f4521dd2b237e
the-phenom <srisathish96@gmail.com>
2016-09-25 09:40:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/my-profile.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'my-prifle.html'));
 });
 
