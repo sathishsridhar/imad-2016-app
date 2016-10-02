01dd5102fc739c7f197eb6969975f9846e7e4e98
the-phenom <srisathish96@gmail.com>
2016-10-02 18:02:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
  
-app.get('/profileName', function (req, res) {
+app.get('/:profileName', function (req, res) {
     var profileName= req.params.profileName;
   res.send(templete(profiles[profileName]));
 });
